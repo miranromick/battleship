ecc711856220f684ab196267f95671f6d700d062
miranromick <miranman@hotmail.com>
2017-04-11 16:45:05 -0700

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -1,27 +1,52 @@
-// var board = [
-//   [1,"","","","","","","","","",""],
-//   [2,"","","","","","","","","",""],
-//   [3,"","","","","","","","","",""],
-//   [4,"","","","","","","","","",""],
-//   [5,"","","","","","","","","",""],
-//   [6,"","","","","","","","","",""],
-//   [7,"","","","","","","","","",""],
-//   [8,"","","","","","","","","",""],
-//   [9,"","","","","","","","","",""],
-//   [10,"","","","","","","","","",""],
-// ]
+var ships = [
+  "ship","ship","ship","ship","ship","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+  "","","","","","","","","","",
+]
+
+// first loop create the rows
+// second loop create the td cells
+//
+for (var x = 0; x < 10; x++) {
+  var createRows = document.querySelector('#gameBoard')
+  var tr = createRows.insertRow(x)
+  tr.id = x
+  for (var y = 0; y < 10; y++) {
+    var createCells = document.getElementById(x)
+    var td = createCells.insertCell(y)
+    td.setAttribute("onclick", "checkForHits(event)")
 
 
-var mytable = '<table><td id="' + 0 + '">';
-var y = 1
-for (var x = 1; x < 101; x++) {
-  if (x % 10 == 1 && x != 1) {
-    y++
-    mytable += '</tr><td id="' + y + '">';
+    td.id = "" + x + y
   }
-  mytable += '<td id="' + x + '">' + '</td>';
 }
 
-mytable += "</tr></table>";
+function checkForHits(e){
+    var coordinate = e.target.id
+    console.log(ships[coordinate])
+    document.getElementById(coordinate).className = "hit"
+}
 
-document.write(mytable);
+// if(ships[this.id] = "ship") {
+//   var coordinate = e.target.id
+//   document.getElementById(coordinate).className = "hit"
+// } else document.getElementById(coordinate).className = "miss"
+// var ships = [
+//   ["ship","ship","ship","ship","ship","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+//   ["","","","","","","","","",""],
+// ]
